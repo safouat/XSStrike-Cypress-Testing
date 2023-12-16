d5cc4ffdde80091df2b0d02b1453b36b5090ebec
SAFOUAT EL YASSINI <120058233+safouat@users.noreply.github.com>
2023-12-16 18:07:41 +0200

    Create xss.cy.js

---
+++ cypress/e2e/xss.cy.js
@@ -0,0 +1,38 @@
+// <reference types="cypress" />
+
+
+describe('URL-based XSS Testing', () => {
+    const targetUrl = 'http://www.sudo.co.il/xss/level6.php?p=your_payload'; // Replace with your target URL
+  
+    it('Test for URL-based XSS Vulnerabilities', () => {
+      cy.visit(targetUrl);
+  
+      // Define an array of payloads for XSSTrike.
+      const payloads = [
+        "\\';confirm()//", "\\';(confirm)()//"    
+        /* Add more payloads as needed, and if your payload starts with a 
+  backslash (''), add two backslashes ('\') in your payloads list instead 
+  of a single backslash, because the backslash character is not typically
+   encrypted itself.*/
+      ];
+  
+      // Loop through the payloads and test the URL with the payloads.
+      payloads.forEach((payload) => {
+        // Construct the URL with the payload.
+        const xssUrl = `${targetUrl}?param=${payload}`;
+  
+        // Visit the URL with the payload and check the response.
+        cy.visit(xssUrl);
+  
+        // You can then use Cypress assertions to check the page for signs of successful XSS.
+        // For example, you might check for the appearance of an alert box triggered by the payload.
+        cy.on('window:confirm', (confirmText) => {
+          if (confirmText) {
+            // Check if confirmText is defined before making assertions.
+            expect(confirmText).to.include('www.sudo.co.il');
+            return onkeydown;
+          }
+        });
+      });
+    });
+  });
